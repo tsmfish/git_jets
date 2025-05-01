b986f877d620c8b84ec7d90bc116d1fbfc04d05e
Pavel Malko <tsmfish@gmail.com>
2025-05-01 03:40:36 +0100

    Add Sky

--- background.py
+++ background.py
@@ -1,5 +1,6 @@
 import pygame
-from config import WIDTH, HEIGHT, SKY_TOP, SKY_BOTTOM
+from config import WIDTH, HEIGHT, SKY_TOP, SKY_BOTTOM, WHITE
+
 
 def create_background():
     background = pygame.Surface((WIDTH, HEIGHT))
@@ -9,6 +10,9 @@ def create_background():
         g = int(SKY_TOP[1] * (1 - t) + SKY_BOTTOM[1] * t)
         b = int(SKY_TOP[2] * (1 - t) + SKY_BOTTOM[2] * t)
         pygame.draw.line(background, (r, g, b), (0, y), (WIDTH, y))
+
+        pygame.draw.circle(background, WHITE, (200, 100), 50, 0)
+        pygame.draw.circle(background, WHITE, (250, 120), 40, 0)
     return background
 
 def draw_background(screen, background):
